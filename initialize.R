338d4cd5ada93a40d6e31e388b7c8eacfc66a831
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2013-06-15 07:39:16 -0400

    workshop

--- initialize.R
+++ initialize.R
@@ -1,15 +1,16 @@
 # Initialization of R for workshop
 
 options(repository='http://cran.rstudio.org')
-install.packages("devtools")
-
-install.packages(c("httr",'RCurl','memoise','whisker'))
 have.packages <- installed.packages()
+
+if(!('devtools' %in% have.packages)) install.packages("devtools")
 cran.packages <- c("reshape2","plyr","ggplot2","Hmisc","rms", "knitr","mlbench","caret",
-                   "sqldf",'RSQLite', 'pander')
+                   "sqldf",'RSQLite', 'pander', 'gdata')
 to.install <- setdiff(cran.packages, have.packages[,1])
 if(length(to.install)>0) install.packages(to.install)
 
-
-ramnath.packages <- c('slidify','rCharts')
-for(p in ramnath.packages) install_github(p, 'ramnathv')
+library(devtools)
+ramnath.packages <- c('slidify','rCharts','slidifyLibraries')
+for(p in ramnath.packages) {
+  if(!(p %in% have.packages)) install_github(p, 'ramnathv')
+}
